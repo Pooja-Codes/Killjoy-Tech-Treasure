dd184878cdfc3a0b0e915e5b3aef772cca3af19e
Pooja Patel <poojapatel@nyu.edu>
2022-11-24 17:40:03 -0500

    Camera script update

--- Killjoy-Tech-Treasure/Assets/Scripts/Camera.cs
+++ Killjoy-Tech-Treasure/Assets/Scripts/Camera.cs
@@ -3,58 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Camera:MonoBehaviour{
-    //the traget
+    // //the traget
    
-    public Transform targetObj;
+    // public Transform targetObj;
+    // //default distance between target and the player
+    // public Vector3 camereaOffset=new Vector3(-4,-5,-15);
 
-    //default distance between target and the player
-    public Vector3 camereaOffset;
+    // //smooth factor will use in Camera rotation
+    // public float smoothFactor=0.7f;
 
-    //smooth factor will use in Camera rotation
-    public float smoothFactor=0.7f;
-
-    //will check that the camera looked at on the target or not.
+    // //will check that the camera looked at on the target or not.
    
-     public bool lookAtTarget=false;
+    //  public bool lookAtTarget=false;
 
 
-    void Start(){
-        //camereaOffset=transform.position-targetObj.transform.position;
-    }
-    void Update(){
-        //look for kj if found targetobj=transform of kj <only if target obj is null>
-        // if (targetObj==null){
-        //     KilljoyObj= GameObject.Find ("Killjoy.glb");
-        //     targetObj=KilljoyObj.transform;
-        // }
-         if (targetObj==null && this.gameObject.name=="Killjoy.glb"){
-            targetObj=this.gameObject.transform;
-            camereaOffset=transform.position-targetObj.transform.position;
-        }
-        if(targetObj!=null){
-            camereaOffset=transform.position-targetObj.transform.position;
-        }
-        
-    }
-    void LateUpdate(){
-        Vector3 newPosition=targetObj.transform.position+camereaOffset;
-        transform.position=Vector3.Slerp(transform.position,newPosition,smoothFactor);
-
-        //Camera Rotation change
-        if(lookAtTarget){
-            transform.LookAt(targetObj);
-        }
-    }
-
-    // public Transform camTarget;
-    // public float pLerp=.02f;
-    // public float rLerp=0.1f;
-
-    // void Update(){
-
-    //     transform.position=Vector3.Lerp(transform.position,camTarget.position,pLerp);
-    //     transform.rotation=Quaternion.Lerp(transform.rotation,camTarget.rotation,rLerp);
+    // void Start(){
+    //     //camereaOffset=transform.position-targetObj.transform.position;
     // }
+    // void Update(){
+    //     //look for kj if found targetobj=transform of kj <only if target obj is null>
+    //     // if (targetObj==null){
+    //     //     KilljoyObj= GameObject.Find ("Killjoy.glb");
+    //     //     targetObj=KilljoyObj.transform;
+    //     // }
+    //     //  if (targetObj==null && this.gameObject.name=="Killjoy.glb"){
+    //     //     targetObj=this.gameObject.transform;
+    //     //     camereaOffset=transform.position-targetObj.transform.position;
+    //     // }
+    //     // if(targetObj!=null){
+    //     //     camereaOffset=transform.position-targetObj.transform.position;
+    //     // }
+        
+    // }
+    // void LateUpdate(){
+    //     Vector3 newPosition=targetObj.transform.position+camereaOffset;
+    //     transform.position=Vector3.Slerp(transform.position,newPosition,smoothFactor);
+
+    //     //Camera Rotation change
+    //     if(lookAtTarget){
+    //         transform.LookAt(targetObj);
+    //     }
+    // }
+
+     //the traget
+    public Transform camTarget;
+    //default distance between target and the player
+    public Vector3 camereaOffset=new Vector3(-4,-5,-15);
+    public float rotationSpeed;
+    //public float pLerp=.02f;
+     public float pLerp=10f;
+    public float rLerp=0.1f;
+    void Start(){
+        camereaOffset=transform.position-camTarget.transform.position;
+    }
+
+    void LateUpdate(){
+        //camereaOffset=transform.position-camTarget.transform.position;
+        Vector3 newPosition=camTarget.transform.position+camereaOffset;
+        transform.position=Vector3.Lerp(transform.position,newPosition,pLerp);
+        transform.rotation=Quaternion.Lerp(transform.rotation,camTarget.rotation,rLerp);
+    }
 
 }
 

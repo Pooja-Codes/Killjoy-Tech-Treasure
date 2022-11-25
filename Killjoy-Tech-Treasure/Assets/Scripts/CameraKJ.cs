3ba1854308cbb019f31adc29f3731ce849f9a737
Pooja Patel <poojapatel@nyu.edu>
2022-11-24 21:06:29 -0500

    Renamed Camera Screen to CameraKJ

--- Killjoy-Tech-Treasure/Assets/Scripts/CameraKJ.cs
+++ Killjoy-Tech-Treasure/Assets/Scripts/CameraKJ.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera:MonoBehaviour{
+public class CameraKJ:MonoBehaviour{
     // //the traget
    
     // public Transform targetObj;

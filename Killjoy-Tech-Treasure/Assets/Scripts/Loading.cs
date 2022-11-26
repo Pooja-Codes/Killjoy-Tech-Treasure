e58ceff48314534b2f54d5dc1ba02fa38bcdccea
Pooja Patel <poojapatel@nyu.edu>
2022-11-25 19:10:11 -0500

    Loading Screen

---
+++ Killjoy-Tech-Treasure/Assets/Scripts/Loading.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Loading : MonoBehaviour
+{
+    // public void Start(){
+    //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    // }
+    // public void LoadScene(int levelIndex)
+    // {
+    //     StartCoroutine(LoadSceneAsynchronously(levelIndex));
+    // }
+
+    // IEnumerator LoadSceneAsynchronously(int levelIndex){
+    //     AsyncOperation operation=SceneManager.LoadSceneAsync(levelIndex);
+    //     while(!operation.isDone)
+    //     {
+    //         Debug.Log(operation.progress);
+    //         yield return null;
+    //     }
+    // }
+
+}
